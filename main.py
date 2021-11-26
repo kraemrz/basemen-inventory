0965c9e49d6b360a422777a1b5276466fcb150b5
kraemrz <kraemrz@gmail.com>
2021-11-26 22:49:46 +0100

    just some testing

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    pass
+    part_nr = input("Enter number on part: ")
+    part_name = input("Name of part: ")
+    add_part(part_nr, part_name)
+    
+
+def add_part(part_nr, part_name):
+    """this function will be responseble for adding parts to the db"""
+    print(part_name, part_nr)
+
+
+
 
 if __name__ == "__main__":
     main()
